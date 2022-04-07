092ff2f532dab92a5ceb1decc6519348b572633c
Fran-Godoy <101577372+Fran-Godoy@users.noreply.github.com>
2022-04-07 08:52:11 -0300

    Excercise 01 & 02 Completion

---
+++ Assets/Scripts/Ej02.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej02 : MonoBehaviour
+{
+    public float gasto1, gasto2, gasto3;
+    public float dinero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float total = gasto1 + gasto2 + gasto3;
+        if (total > dinero)
+        {
+           Debug.Log("Te faltan " + (total - dinero) + " pesos");
+        }
+        else
+        {
+            Debug.Log("Te sobran " + (dinero - total) + " pesos");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
